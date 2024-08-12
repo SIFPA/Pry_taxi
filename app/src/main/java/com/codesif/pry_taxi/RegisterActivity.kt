deab7024cf2369bc4eeb429b57a324382b1ad10c
CodeSif <pablosifuentescacha@gmail.com>
2024-08-11 22:22:02 -0500

    Diseño del RegisterActivity.kt y los intent para pasar el main al register

--- app/src/main/java/com/codesif/pry_taxi/RegisterActivity.kt
+++ app/src/main/java/com/codesif/pry_taxi/RegisterActivity.kt
@@ -2,34 +2,31 @@ package com.codesif.pry_taxi
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.codesif.pry_taxi.databinding.ActivityMainBinding
+import com.codesif.pry_taxi.databinding.ActivityRegisterBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         enableEdgeToEdge()
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        binding.btnRegister.setOnClickListener { goToRegister() }
+        binding.btnLogin.setOnClickListener { goToLogin() }
+
     }
 
-    private fun goToRegister() {
-        val i = Intent(this, RegisterActivity::class.java)
+    private fun goToLogin() {
+        val i = Intent(this, MainActivity::class.java)
         startActivity(i)
     }
-
 }
